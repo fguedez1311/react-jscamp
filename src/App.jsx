2c27041cb8aaec1dbe0486306822c726d53fe277
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2026-01-29 16:08:07 -0400

    feat:Creado un componente declarativo

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
-import { useEffect, useState } from "react";
-
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 
 import { HomePage } from "./pages/Home";
 import { SearchPage } from "./pages/SearchPage";
-import { NoFoundPage } from "./pages/404";
-import { useRouter } from "./hooks/useRouter";
+import { Route } from "./components/Route";
 
 function App() {
-  const {currentPath}=useRouter()
-
-  let page = <NoFoundPage />;
-  if (currentPath === "/") {
-    page = <HomePage />;
-  } else if (currentPath === "/search") {
-    page = <SearchPage />;
-  }
+  
   
   return (
     <>
       <Header />
-      {page}
+      <Route path="/" component={HomePage} />
+      <Route path="/search" component={SearchPage} />
 
       <Footer />
     </>
